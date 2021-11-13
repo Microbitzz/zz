81352f2822726ba7cb094028a863fb5e04c4ca62
Microbitzz <93979252+Microbitzz@users.noreply.github.com>
2021-11-13 17:45:50 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -8,23 +8,51 @@ def on_button_pressed_a():
     robotbit.motor_run(robotbit.Motors.M1B, 255)
     robotbit.motor_run(robotbit.Motors.M2A, 255)
     robotbit.motor_run(robotbit.Motors.M2B, 255)
+    robotbit.rgb().show_color(neopixel.rgb(30, 2, 2))
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_uart_data_received():
+    basic.show_string("Z-1")
+bluetooth.on_uart_data_received("1", on_uart_data_received)
 
 def on_button_pressed_b():
     robotbit.motor_run(robotbit.Motors.M1A, -255)
     robotbit.motor_run(robotbit.Motors.M1B, -255)
     robotbit.motor_run(robotbit.Motors.M2A, -255)
     robotbit.motor_run(robotbit.Motors.M2B, -255)
+    robotbit.rgb().show_color(neopixel.rgb(2, 2, 30))
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_sound_loud():
     basic.show_icon(IconNames.HEART)
-    robotbit.rgb().show_color(neopixel.rgb(100, 100, 100))
+    robotbit.rgb().show_color(neopixel.rgb(30, 30, 30))
     basic.pause(1000)
     basic.show_icon(IconNames.SMALL_HEART)
-    robotbit.rgb().show_color(neopixel.rgb(2, 100, 9))
+    robotbit.rgb().show_color(neopixel.rgb(2, 30, 2))
+    bluetooth.uart_write_value("x", 0)
 input.on_sound(DetectedSound.LOUD, on_sound_loud)
 
-radio.set_group(0)
+def on_mes_dpad_controller_id_microbit_evt():
+    if control.event_value() % 2 == 1:
+        led.plot(Math.floor((control.event_value() - 1 - Math.floor((control.event_value() - 1) / 8) * 8) / 2),
+            Math.floor((control.event_value() - 1) / 8))
+    else:
+        led.unplot(Math.floor((control.event_value() - 1 - Math.floor((control.event_value() - 1) / 8) * 8) / 2),
+            Math.floor((control.event_value() - 1) / 8))
+control.on_event(EventBusSource.MES_DPAD_CONTROLLER_ID,
+    EventBusValue.MICROBIT_EVT_ANY,
+    on_mes_dpad_controller_id_microbit_evt)
+
+def on_uart_data_received2():
+    basic.show_string("Z-A")
+bluetooth.on_uart_data_received("A", on_uart_data_received2)
+
 input.set_sound_threshold(SoundThreshold.LOUD, 150)
 basic.show_number(0)
+bluetooth.start_accelerometer_service()
+bluetooth.start_button_service()
+bluetooth.start_io_pin_service()
+bluetooth.start_led_service()
+bluetooth.start_temperature_service()
+bluetooth.start_magnetometer_service()
+bluetooth.start_uart_service()
--- main.ts
+++ main.ts
@@ -1,20 +1,53 @@
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    robotbit.MotorStopAll()
-    basic.showIcon(IconNames.Happy)
-})
-input.onButtonPressed(Button.A, function () {
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_4_DOWN, function () {
     robotbit.MotorRun(robotbit.Motors.M1A, 255)
     robotbit.MotorRun(robotbit.Motors.M1B, 255)
-    robotbit.MotorRun(robotbit.Motors.M2A, 255)
-    robotbit.MotorRun(robotbit.Motors.M2B, 255)
-    robotbit.rgb().showColor(neopixel.rgb(30, 2, 2))
+    robotbit.MotorRun(robotbit.Motors.M2A, -255)
+    robotbit.MotorRun(robotbit.Motors.M2B, -255)
 })
-input.onButtonPressed(Button.B, function () {
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_4_UP, function () {
+    robotbit.MotorStopAll()
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_C_DOWN, function () {
+    robotbit.MotorRun(robotbit.Motors.M1A, 255)
+    robotbit.MotorRun(robotbit.Motors.M1B, -255)
+    robotbit.MotorRun(robotbit.Motors.M2A, 255)
+    robotbit.MotorRun(robotbit.Motors.M2B, -255)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_3_UP, function () {
+    robotbit.MotorStopAll()
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_D_DOWN, function () {
+    robotbit.MotorRun(robotbit.Motors.M1A, -255)
+    robotbit.MotorRun(robotbit.Motors.M1B, 255)
+    robotbit.MotorRun(robotbit.Motors.M2A, -255)
+    robotbit.MotorRun(robotbit.Motors.M2B, 255)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_B_DOWN, function () {
     robotbit.MotorRun(robotbit.Motors.M1A, -255)
     robotbit.MotorRun(robotbit.Motors.M1B, -255)
     robotbit.MotorRun(robotbit.Motors.M2A, -255)
     robotbit.MotorRun(robotbit.Motors.M2B, -255)
-    robotbit.rgb().showColor(neopixel.rgb(2, 2, 30))
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_A_DOWN, function () {
+    robotbit.MotorRun(robotbit.Motors.M1A, 255)
+    robotbit.MotorRun(robotbit.Motors.M1B, 255)
+    robotbit.MotorRun(robotbit.Motors.M2A, 255)
+    robotbit.MotorRun(robotbit.Motors.M2B, 255)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_B_UP, function () {
+    robotbit.MotorStopAll()
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_3_DOWN, function () {
+    robotbit.MotorRun(robotbit.Motors.M1A, -255)
+    robotbit.MotorRun(robotbit.Motors.M1B, -255)
+    robotbit.MotorRun(robotbit.Motors.M2A, 255)
+    robotbit.MotorRun(robotbit.Motors.M2B, 255)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_D_UP, function () {
+    robotbit.MotorStopAll()
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_C_UP, function () {
+    robotbit.MotorStopAll()
 })
 input.onSound(DetectedSound.Loud, function () {
     basic.showIcon(IconNames.Heart)
@@ -22,7 +55,27 @@ input.onSound(DetectedSound.Loud, function () {
     basic.pause(1000)
     basic.showIcon(IconNames.SmallHeart)
     robotbit.rgb().showColor(neopixel.rgb(2, 30, 2))
+    bluetooth.uartWriteValue("x", 0)
 })
-radio.setGroup(0)
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_2_DOWN, function () {
+    geekservo += -15
+    robotbit.GeekServo(robotbit.Servos.S1, geekservo)
+    robotbit.GeekServo(robotbit.Servos.S2, geekservo)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_A_UP, function () {
+    robotbit.MotorStopAll()
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_1_DOWN, function () {
+    geekservo += 15
+    robotbit.GeekServo(robotbit.Servos.S1, geekservo)
+    robotbit.GeekServo(robotbit.Servos.S2, geekservo)
+})
+let geekservo = 0
 input.setSoundThreshold(SoundThreshold.Loud, 150)
-basic.showNumber(0)
+basic.showNumber(EventBusSource.MES_DPAD_CONTROLLER_ID)
+bluetooth.startAccelerometerService()
+bluetooth.startButtonService()
+bluetooth.startIOPinService()
+bluetooth.startLEDService()
+bluetooth.startTemperatureService()
+bluetooth.startMagnetometerService()
